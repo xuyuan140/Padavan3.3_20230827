93465871e4329d78adf767f14b2be0341774dced
xuyuan140 <xubin@vip.qq.com>
2023-08-27 03:11:52 +0800

    Update board.h

--- trunk/configs/boards/7620A-VPNwifi/board.h
+++ trunk/configs/boards/7620A-VPNwifi/board.h
@@ -1,8 +1,8 @@
 /* NEWIFI3(d2) */
 
-#define BOARD_PID		"NEWIFI3"
-#define BOARD_NAME		"NEWIFI3"
-#define BOARD_DESC		"NEWIFI3 Wireless Router"
+#define BOARD_PID		"7620A-VPNwifi"
+#define BOARD_NAME		"7620A-VPNwifi"
+#define BOARD_DESC		"7620A-VPNwifi Wireless Router"
 #define BOARD_VENDOR_NAME	"Diting Technology"
 #define BOARD_VENDOR_URL	"http://www.newifi.com/"
 #define BOARD_MODEL_URL		"http://www.newifi.com/"
